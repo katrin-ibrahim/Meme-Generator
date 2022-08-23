61683fb95e3fa194e359ca699fc9ab05d749b12b
katrin-ibrahim <katrinemadfathy@gmail.com>
2022-08-23 15:11:19 +0200

    added meme text with form functionality

--- src/componenets/Meme.jsx
+++ src/componenets/Meme.jsx
@@ -8,6 +8,9 @@ export default function Meme() {
         bottomText:"",
         randomImage:"https://i.imgflip.com/39t1o.jpg"
     })
+
+  
+    
     const [allMemeImages,setAllMemeImages] = useState(memesData)
     
   
@@ -23,29 +26,49 @@ export default function Meme() {
             ...prevMeme,
             randomImage: url
         }))
-        console.log("hahhah")
+     
 
     }
+
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
   return (
    <main>
-     <img 
-        src={meme.randomImage}
-        className="meme-image"
-    />
+     <div className='meme'>
+         <img
+            src={meme.randomImage}
+            className="meme-image"
+             />
+             <h2 className="meme-text top">{meme.topText}</h2>
+                <h2 className="meme-text bottom">{meme.bottomText}</h2>
+     </div>
 
-
-     
         <div className='form'>
         <span className='intro-text'><strong>Welcome to Meme Generator!</strong><br/>Click "Generate a new  meme image !” to get a new random meme , then customize it with any text you like!</span>
             <input
                 type="text"
                 className='form-input'
                 placeholder='Top text'
+                name="topText"
+                value={meme.topText}
+                onChange={handleChange}
+          
              />
             <input
                 type="text"
                 className='form-input'
                 placeholder='Bottom text'
+                name="bottomText"
+                value={meme.bottomText}
+                onChange={handleChange}
+                
             />
       
             <button
